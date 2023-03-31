448f47729220ce017033df510e9a2006ef1393a9
Airat Vagapov <airlake@yandex.ru>
2023-03-31 22:37:49 +0400

    finish landing, start modals

--- local/assets/js/slider.js
+++ local/assets/js/slider.js
@@ -1,25 +1,38 @@
 $(document).ready(function () {
     // Слайдер на блок 8
-    $('.slider').slick({
-        arrows: false,
-        dots: false,
-        prevArrow: $('.button__customArrow--prev--small'),
-        fwdArrow: $('.button__customArrow--fwd')
-    });
 
-    $('.button__customArrow--prev--small').click(function () {
-        $('.slider').slick('slickPrev');
-    });
+    $('.slider').each(function () {
+        var slider = $(this);
+        $(this).slick({
+            arrows: false,
+            dots: false,
+            prevArrow: $('.button__customArrow--prev--small'),
+            fwdArrow: $('.button__customArrow--fwd')
+        });
+        var parent = $(this).closest('.slider__container')
 
-    $('.button__customArrow--fwd').click(function () {
-        $('.slider').slick('slickNext');
-    });
 
-    $('.slider').on('afterChange', function (event, slick, currentSlide) {
-        var counter = $('.slider__container .slider__customCounter');
-        counter.find('.slider__customCounter--current').text('0' + (currentSlide + 1));
-        counter.find('.slider__customCounter--all').text('/' + '0' + slick.slideCount);
-    });
+        parent.find('.button__customArrow--prev--small').click(function () {
+            slider.slick('slickPrev');
+        });
+
+        parent.find('.button__customArrow--fwd').click(function () {
+            slider.slick('slickNext');
+        });
+
+        slider.on('init', function (event, slick, currentSlide) {
+            var counter = parent.find('.slider__customCounter');
+            counter.find('.slider__customCounter--current').text('0' + (currentSlide + 1));
+            counter.find('.slider__customCounter--all').text('/' + '0' + slick.slideCount);
+        });
+        slider.on('afterChange', function (event, slick, currentSlide) {
+            var counter = parent.find('.slider__customCounter');
+            counter.find('.slider__customCounter--current').text('0' + (currentSlide + 1));
+            counter.find('.slider__customCounter--all').text('/' + '0' + slick.slideCount);
+        });
+    })
+
+
 
     // Слайдер на блок 9
     gsap.registerPlugin(ScrollTrigger);
@@ -34,11 +47,28 @@ $(document).ready(function () {
         }
     })
     slideTimeline
-        .fromTo(`[data-slide="1"]`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -10, delay: 0.5 })
+        .fromTo(`[data-slide="1"]`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -10, delay: 2 })
         .fromTo(('[data-slide="2"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0 })
-        .to('[data-slide="2"]', { opacity: 0, yPercent: -10, delay: 0.5 })
+        .to('[data-slide="2"]', { opacity: 0, yPercent: -10, delay: 2 })
         .fromTo(('[data-slide="3"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0 })
-        .to('[data-slide="3"]', { opacity: 0, yPercent: -10, delay: 0.5 })
+        .to('[data-slide="3"]', { opacity: 0, yPercent: -10, delay: 2 })
         .fromTo(('[data-slide="4"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0 })
+
+    // Слайдер блок 18
+
+    let slideTimeline18 = gsap.timeline({
+        scrollTrigger: {
+            // markers: true,
+            trigger: ".verticalSlider--trigger--2",
+            scrub: true,
+            pin: true,
+            start: "top",
+        }
+    })
+    slideTimeline18
+        .fromTo(`[data-slide-2="1"]`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -10, delay: 2, duration: 2 })
+        .fromTo(('[data-slide-2="2"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0, duration: 2 })
+        .to('[data-slide-2="2"]', { opacity: 0, yPercent: -10, delay: 2, duration: 1 })
+        .fromTo(('[data-slide-2="3"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0, duration: 2 })
 
 })
