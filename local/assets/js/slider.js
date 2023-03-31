35b7bcbb4ad3df6488a6e8c01db41ce3573b8ce5
Airat Vagapov <airlake@yandex.ru>
2023-03-31 12:37:54 +0400

    vertical slider

--- local/assets/js/slider.js
+++ local/assets/js/slider.js
@@ -24,22 +24,9 @@ $(document).ready(function () {
     // Слайдер на блок 9
     gsap.registerPlugin(ScrollTrigger);
 
-    // const slides = gsap.utils.toArray('.verticalSlider__slide');
-
-    // slides.forEach((slide, index) => {
-    //     const slideTimeline = gsap.timeline({
-    //         defaults: { duration: 1 }
-    //     })
-    //     // slideTimeline.fromTo(slide, { yPercent: -100 }, { yPercent: 0 })
-    //     //     .to(slide, { yPercent: 100, duration: 0.5, delay: 1 });
-
-    //     slideTimeline
-    //         .fromTo(`.verticalSlider__slide:first-child`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -100 }, `silde${index}`)
-    //         .fromTo(('.verticalSlider__slide'), { opacity: 0, yPercent: 100 }, { opacity: 1, yPercent: 0 }, `silde${index}`)
-
     let slideTimeline = gsap.timeline({
         scrollTrigger: {
-            markers: true,
+            // markers: true,
             trigger: ".verticalSlider--trigger",
             scrub: true,
             pin: true,
@@ -47,38 +34,11 @@ $(document).ready(function () {
         }
     })
     slideTimeline
-        .fromTo(`[data-slide="1"]`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -100 }, `slide1`)
-        .fromTo(('[data-slide="2"]'), { opacity: 0, yPercent: 100 }, { opacity: 1, yPercent: -100 }, `slide2`)
-        .fromTo(('[data-slide="3"]'), { opacity: 0, yPercent: 0 }, { opacity: 1, yPercent: 0 }, `slide3`)
-        .fromTo(('[data-slide="4"]'), { opacity: 0, yPercent: 0 }, { opacity: 1, yPercent: 0 }, `slide4`)
+        .fromTo(`[data-slide="1"]`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -10, delay: 0.5 })
+        .fromTo(('[data-slide="2"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0 })
+        .to('[data-slide="2"]', { opacity: 0, yPercent: -10, delay: 0.5 })
+        .fromTo(('[data-slide="3"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0 })
+        .to('[data-slide="3"]', { opacity: 0, yPercent: -10, delay: 0.5 })
+        .fromTo(('[data-slide="4"]'), { opacity: 0, yPercent: 10 }, { opacity: 1, yPercent: 0 })
 
-    // ScrollTrigger.create({
-    //     trigger: ".verticalSlider--trigger",
-    //     // start: `+=${index * 800}px`,
-    //     start: `top top`,
-    //     end: `3200px`,
-    //     animation: slideTimeline,
-    //     // scrub: true,
-    //     pin: true,
-    //     pinSpacing: false,
-    //     snap: "labels"
-    // });
-
-    // var tl = gsap.timeline({
-    //     scrollTrigger: {
-    //         markers: true,
-    //         trigger: ".planets_anim_container",
-    //         scrub: true,
-    //         pin: true,
-    //         start: "top 10%",
-    //     }
-    // });
-    // tl
-    //     .add('animation1')
-    //     .to(".planets_anim__dots", { scale: 1, duration: 3, opacity: 1 }, 'animation1')
-    //     .to(".planets_anim__circles", { scale: 1, duration: 3 }, 'animation1')
-    //     .add('animation2')
-    //     .to(".planets_anim__dots>span", { skewX: 51, skewY: -24, duration: 3, scale: 0.63 }, 'animation2')
-    //     .to(".planets_anim__dots", { skewX: -51, skewY: 24, duration: 3 }, 'animation2')
-    //     .to(".planets_anim__circles", { skewX: -51, skewY: 24, duration: 3 }, 'animation2')
 })
