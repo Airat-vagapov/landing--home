39f37bbc9319f0c401f1c30f0ecb51f41080326a
Airat Vagapov <airlake@yandex.ru>
2023-03-31 12:13:41 +0400

    anim

--- local/assets/js/slider.js
+++ local/assets/js/slider.js
@@ -16,63 +16,69 @@ $(document).ready(function () {
     });
 
     $('.slider').on('afterChange', function (event, slick, currentSlide) {
-        var counter = $(this).find('.slider__customCounter');
+        var counter = $('.slider__container .slider__customCounter');
         counter.find('.slider__customCounter--current').text('0' + (currentSlide + 1));
         counter.find('.slider__customCounter--all').text('/' + '0' + slick.slideCount);
     });
 
     // Слайдер на блок 9
-    $('.slider--2').slick({
-        arrows: false,
-        vertical: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        verticalSwiping: true,
-        infinite: false,
-        // afterChange: function (event, slick, currentSlide, nextSlide) {
-        //     console.log('yes')
-        //     var textBlock = $('.textBlock[data-slide="1"]')
-        //     console.log(textBlock)
+    gsap.registerPlugin(ScrollTrigger);
 
-        //     textBlock.fadeOut(200, function () {
-        //         textBlock.text('New slide: ' + (nextSlide + 1));
-        //         textBlock.fadeIn(200);
-        //     })
-        // }
-    })
+    // const slides = gsap.utils.toArray('.verticalSlider__slide');
 
-    $('.slider--2').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    // slides.forEach((slide, index) => {
+    //     const slideTimeline = gsap.timeline({
+    //         defaults: { duration: 1 }
+    //     })
+    //     // slideTimeline.fromTo(slide, { yPercent: -100 }, { yPercent: 0 })
+    //     //     .to(slide, { yPercent: 100, duration: 0.5, delay: 1 });
 
-        var textBlock = $(`.textBlock--2[data-slide="${currentSlide}"]`)
+    //     slideTimeline
+    //         .fromTo(`.verticalSlider__slide:first-child`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -100 }, `silde${index}`)
+    //         .fromTo(('.verticalSlider__slide'), { opacity: 0, yPercent: 100 }, { opacity: 1, yPercent: 0 }, `silde${index}`)
 
-        textBlock.slideUp(200, function () {
-            var nextBlock = $(`.textBlock--2[data-slide="${currentSlide + 1}"]`)
-            nextBlock.removeClass('slider__text--hidden')
-            nextBlock.addClass('slider__text--active')
-            nextBlock.slideUp(200)
-        })
-    })
-
-    $(document).on('mousewheel', function (event) {
-        // event.preventDefault();
-
-        if (scrolling) return;
-
-        scrolling = true;
-
-        if (event.deltaY > 0) {
-            $('.slider--2').slick('slickPrev');
-
-        } else {
-            $('.slider--2').slick('slickNext');
+    let slideTimeline = gsap.timeline({
+        scrollTrigger: {
+            markers: true,
+            trigger: ".verticalSlider--trigger",
+            scrub: true,
+            pin: true,
+            start: "top",
         }
+    })
+    slideTimeline
+        .fromTo(`[data-slide="1"]`, { opacity: 1, y: 0 }, { opacity: 0, yPercent: -100 }, `slide1`)
+        .fromTo(('[data-slide="2"]'), { opacity: 0, yPercent: 100 }, { opacity: 1, yPercent: -100 }, `slide2`)
+        .fromTo(('[data-slide="3"]'), { opacity: 0, yPercent: 0 }, { opacity: 1, yPercent: 0 }, `slide3`)
+        .fromTo(('[data-slide="4"]'), { opacity: 0, yPercent: 0 }, { opacity: 1, yPercent: 0 }, `slide4`)
 
-        setTimeout(function () {
-            scrolling = false;
-        }, 500);
-    });
+    // ScrollTrigger.create({
+    //     trigger: ".verticalSlider--trigger",
+    //     // start: `+=${index * 800}px`,
+    //     start: `top top`,
+    //     end: `3200px`,
+    //     animation: slideTimeline,
+    //     // scrub: true,
+    //     pin: true,
+    //     pinSpacing: false,
+    //     snap: "labels"
+    // });
 
-
-
-
-});
+    // var tl = gsap.timeline({
+    //     scrollTrigger: {
+    //         markers: true,
+    //         trigger: ".planets_anim_container",
+    //         scrub: true,
+    //         pin: true,
+    //         start: "top 10%",
+    //     }
+    // });
+    // tl
+    //     .add('animation1')
+    //     .to(".planets_anim__dots", { scale: 1, duration: 3, opacity: 1 }, 'animation1')
+    //     .to(".planets_anim__circles", { scale: 1, duration: 3 }, 'animation1')
+    //     .add('animation2')
+    //     .to(".planets_anim__dots>span", { skewX: 51, skewY: -24, duration: 3, scale: 0.63 }, 'animation2')
+    //     .to(".planets_anim__dots", { skewX: -51, skewY: 24, duration: 3 }, 'animation2')
+    //     .to(".planets_anim__circles", { skewX: -51, skewY: 24, duration: 3 }, 'animation2')
+})
